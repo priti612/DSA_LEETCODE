d6526852938e73d6e323126cd745ef60483494db
Priti Prabha <jdwomenscoll@gmail.com>
2026-01-18 22:21:24 +0530

    Time: 44 ms (81.68%), Space: 18.6 MB (56%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,21 +1,22 @@
 class Solution:
-    def findTargetSumWays(self, arr: List[int], diff: int) -> int:
-        # def subset(arr,tag):
-        def countsub(arr,summ):
-            n=len(arr)
-            t=[[0]*(summ+1) for _ in range(n+1)]
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        def knp(nums,targ):
+            n=len(nums)
+            t=[[0]*(targ+1) for _ in range(n+1)]
             t[0][0]=1
             for i in range(1,n+1):
-                for j in range(summ+1):
-                    if arr[i-1]<=j:
-                        t[i][j]=t[i-1][j-arr[i-1]] + t[i-1][j]
+                for j in range(targ+1):
+                    if nums[i-1]<=j:
+                        t[i][j]=t[i-1][j-nums[i-1]]+t[i-1][j]
                     else:
                         t[i][j]=t[i-1][j]
-            return t[n][summ]
-        n=len(arr)
-        s1=sum(arr)
-        sum_s1=(diff+s1)//2
-        if (diff +s1) % 2 != 0 or abs(diff)>s1:
+            return t[n][targ]
+        n=len(nums)
+        s1=sum(nums)
+        targ=(target+s1)//2
+        if (target+s1)%2!=0 or abs(target)>s1:
             return 0
-        return countsub(arr,sum_s1)
-        
+        return knp(nums,targ)
+
+
+

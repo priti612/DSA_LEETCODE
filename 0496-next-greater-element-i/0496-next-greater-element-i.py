3a2180eab72e1695496707f7219dc3b63b97ce3f
Priti Prabha <jdwomenscoll@gmail.com>
2026-02-17 22:22:41 +0530

    Time: 5 ms (23.19%), Space: 19.4 MB (50.91%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -2,17 +2,18 @@ class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         n=len(nums1)
         m=len(nums2)
-        res=[-1]*n
         st=[]
-        fre={}
-        for i in range(m-1,-1,-1):
-            curr=nums2[i]
-            while st and st[-1]<=nums2[i]:
-                st.pop()
-            if not st:
-                fre[curr]=-1
-            else:
-                fre[curr]=st[-1]
-            st.append(curr)
-        return [fre[i] for i in nums1]
-        
+        res=[]
+        nge={}
+        for i in reversed(nums2):
+                while st and st[-1]<i:
+                    st.pop()
+                if st:
+                    nge[i]=st[-1]
+                else:
+                    nge[i]=-1
+                st.append(i)
+        for i in nums1:
+            res.append(nge[i])
+        return res
+

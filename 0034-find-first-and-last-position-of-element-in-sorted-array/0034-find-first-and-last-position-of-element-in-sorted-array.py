e11cf37efeb06a1a1a73615b254ef417fcea8250
Priti Prabha <jdwomenscoll@gmail.com>
2026-04-30 23:59:53 +0530

    Time: 0 ms (100%), Space: 20.6 MB (28.37%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,39 +1,36 @@
-class Solution(object):
-    def searchRange(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        def first(nums,target):
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        def first(nums,targ):
             res=-1
-            low,high=0,len(nums)-1
+            low=0
+            high=len(nums)-1
             while low<=high:
-                mid=(low+high)//2
-                if nums[mid]==target:
+                mid=low+(high-low)//2
+                if nums[mid]==targ:
                     res=mid
                     high=mid-1
-                elif nums[mid]<target:
+                elif nums[mid]<targ:
                     low=mid+1
                 else:
                     high=mid-1
             return res
-        def last(nums,target):
+        def second(nums,targ):
             res=-1
-            low,high=0,len(nums)-1
+            low=0
+            high=len(nums)-1
             while low<=high:
-                mid=(low+high)//2
-                if nums[mid]==target:
+                mid=low+(high-low)//2
+                if nums[mid]==targ:
                     res=mid
                     low=mid+1
-                elif nums[mid]<target:
+                elif nums[mid]<targ:
                     low=mid+1
                 else:
                     high=mid-1
             return res
-        lb = first(nums, target)
-        ub = last(nums, target)
-        return [lb, ub]
+
+
+        lb=first(nums,target)
+        ub=second(nums,target)
+        return[lb,ub]
         
-
-

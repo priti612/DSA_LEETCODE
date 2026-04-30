a32d518e2f95798a2f3ad49ecad7709dac3f4308
Priti Prabha <jdwomenscoll@gmail.com>
2026-04-30 08:50:19 +0530

    Time: 0 ms (100%), Space: 19.4 MB (16.98%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,16 +1,12 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # if n<=1:
-        #     return 1
-        # return self.climbStairs(n-1)+self.climbStairs(n-2)
-        def sol(n):
-            if r[n]!=-1:
-                return r[n]
-            r[n]=sol(n-1)+sol(n-2)
-            return r[n]
-
-        r=[-1]*(n+1)
-        r[0]=1
-        r[1]=1
-        return sol(n)
         
+        def sol(n):
+            if t[n]!=-1:
+                return t[n]
+            t[n]=sol(n-1)+sol(n-2)
+            return t[n]
+        t=[-1]*(n+1)
+        t[0]=1
+        t[1]=1
+        return sol(n)

268bf680e66804459fedeef2e191275eab29640f
Priti Prabha <jdwomenscoll@gmail.com>
2026-04-30 13:19:09 +0530

    Time: 39 ms (79.34%), Space: 19.4 MB (60.24%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,22 +1,20 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        def knp(nums,targ):
+        def tgsm(nums,target):
             n=len(nums)
-            t=[[0]*(targ+1) for _ in range(n+1)]
+            t=[[0]*(target+1) for _ in range(n+1)]
             t[0][0]=1
             for i in range(1,n+1):
-                for j in range(targ+1):
+                for j in range(target+1):
                     if nums[i-1]<=j:
-                        t[i][j]=t[i-1][j-nums[i-1]]+t[i-1][j]
+                        t[i][j]=t[i-1][j] + t[i-1][j-nums[i-1]]
                     else:
                         t[i][j]=t[i-1][j]
-            return t[n][targ]
-        n=len(nums)
-        s1=sum(nums)
-        targ=(target+s1)//2
-        if (target+s1)%2!=0 or abs(target)>s1:
+            return t[n][target]
+        sm=sum(nums)
+        tg=(sm+target)//2
+        if (sm+target)%2!=0 or abs(tg)>sm:
             return 0
-        return knp(nums,targ)
-
-
-
+        if tg<0:
+            return 0
+        return tgsm(nums,tg)

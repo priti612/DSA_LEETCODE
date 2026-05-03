c6e288d6190efbe08cffef17c0296bf719983bfe
Priti Prabha <jdwomenscoll@gmail.com>
2026-05-03 14:58:31 +0530

    Time: 7 ms (66.72%), Space: 20.9 MB (28.24%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,23 +1,24 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        
+        inc=[0]*(n+1)
         gp=defaultdict(list)
-        for f,t,cost in flights:
-            gp[f].append((t,cost))
-        print(gp)
-        heap=[(0,src,0)]
-        vis={}
-        while heap:
-            ct,node,st=heapq.heappop(heap)
+        for st,end,p in flights:
+            gp[st].append((end,p))
+        h=[(0,src,0)] 
+        dp={} 
+        while h:
+            ct,node,st=heapq.heappop(h)
             if node==dst:
                 return ct
             if st>k:
-                continue
-            if node in vis and vis[node]<=st:
-                continue
+                continue 
             
-            vis[node]=st
-             
-            for nei,p in gp[node]:
-                heappush(heap,(ct+p,nei,st+1))
+            if node in dp and dp[node]<=st:
+                continue 
+            dp[node]=st
+            for nei , p in gp[node]:
+                heappush(h,(ct+p,nei,st+1))
         return -1
+
 

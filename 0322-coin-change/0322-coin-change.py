dcc662b663feea587f806a3d723cd69e59a3f961
Priti Prabha <jdwomenscoll@gmail.com>
2026-06-05 16:05:58 +0530

    Time: 583 ms (40.58%), Space: 19.6 MB (52.27%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,17 +1,15 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        maxi=amount+1
         n=len(coins)
-        t=[[maxi]*(amount+1) for _ in range(n+1)]
-
-        for i in range(n+1):
-            t[i][0]=0
-        for i in range(1,n+1):
-            for j in range(1,amount+1):
-                if coins[i-1]<=j:
-                    t[i][j]=min(t[i-1][j],1+t[i][j-coins[i-1]])
-                else:
-                    t[i][j]=t[i-1][j]
-        ans=t[n][amount]
-        return ans if ans<=amount else -1
+        maxi=float('inf')
+        t=[0]*(amount+1)
+        
+        for i in range(1,amount+1):
+            t[i]=maxi
+            for c in coins:
+                if c<=i and t[i-c]!=maxi:
+                    t[i]=min(1+t[i-c],t[i])
+                
+        res=t[amount]
+        return res if res!=maxi else -1
                                                                                          

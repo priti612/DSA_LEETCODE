83d64b93fc1249aaad96a342ed4f9cc90a79399c
Priti Prabha <jdwomenscoll@gmail.com>
2026-06-05 12:02:11 +0530

    Time: 353 ms (62.52%), Space: 44.2 MB (74.12%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -3,9 +3,6 @@ class Solution:
         n=len(text1)
         m=len(text2)
         t=[[0]*(m+1) for _ in range(n+1)]
-        for i in range(n+1):
-            for j in range(m+1):
-                t[i][j]=0
         for i in range(1,n+1):
             for j in range(1,m+1):
                 if text1[i-1]==text2[j-1]:

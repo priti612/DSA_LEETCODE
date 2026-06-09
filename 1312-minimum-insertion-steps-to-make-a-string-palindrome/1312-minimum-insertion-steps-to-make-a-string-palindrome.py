19fb52579679cce07d09596b7c4b8740ece84dc9
Priti Prabha <jdwomenscoll@gmail.com>
2026-06-09 15:39:30 +0530

    Time: 427 ms (50.54%), Space: 21.3 MB (50.64%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -8,6 +8,6 @@ class Solution:
                 if s[i-1]==s1[j-1]:
                     t[i][j]=1+t[i-1][j-1]
                 else:
-                    t[i][j]=max(t[i][j-1],t[i-1][j])
+                    t[i][j]=max(t[i-1][j],t[i][j-1])
         lcs=t[n][n]
         return n-lcs

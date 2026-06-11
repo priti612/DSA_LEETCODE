6aaa5931d2b5775d28d44120a10664a5f72ac798
Priti Prabha <jdwomenscoll@gmail.com>
2026-06-11 22:37:12 +0530

    Time: 4 ms (10.15%), Space: 19.6 MB (8.79%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,14 +1,39 @@
-class Solution(object):
-    def strStr(self, haystack, needle):
-        """
-        :type haystack: str
-        :type needle: str
-        :rtype: int
+class Solution:
+    def lpsss(self,pat,m,lps):
+            l=0
+            lps[0]=0
+            i=1
+            while i<m:
+                if pat[i]==pat[l]:
+                    l+=1
+                    lps[i]=l
+                    i+=1
+                else:
+                    if l!=0:
+                        l=lps[l-1]
+                        
+                    else:
+                        lps[i]=0
+                        i+=1
+    def strStr(self, txt: str, pat: str) -> int:
         
-        """
-        # pre=needle[0]
-        # for s in haystack:
-        #     while not s.startswith(pre):
-        #         return -1
-        # return 0
-        return haystack.find(needle)
+        n=len(txt)
+        m=len(pat)
+        lps=[0]*m
+        if not pat:
+            return 0
+        self.lpsss(pat,m,lps)
+        i,j=0,0
+        while i<n:
+            if txt[i]==pat[j]:
+                i+=1
+                j+=1
+                if j==m:
+                    return i-j
+                    j=lps[j-1]
+            else:
+                if j!=0:
+                    j=lps[j-1]
+                else:
+                    i+=1
+        return -1

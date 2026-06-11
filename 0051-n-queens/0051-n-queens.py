90ba270fae206b814a74fea6787861d4c62e253c
Priti Prabha <jdwomenscoll@gmail.com>
2026-06-11 13:39:56 +0530

    Time: 15 ms (35.15%), Space: 19.7 MB (69.04%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,17 +1,15 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         res=[]
-        b=[['.']*n for i in range(n)]
+        b=[['.']*n for _ in range(n)]
         row,col,pos,neg=set(),set(),set(),set()
         def sol(i):
-            # temp=[]
             if i==n:
                 res.append(["".join(p) for p in b])
-                return
             for j in range(n):
-                if i in row or j in col or i+j in pos or i-j in neg or b[i][j]=='Q':
+                if i in row or j in col or i+j in pos or i-j in neg or b[i][j]=="Q":
                     continue
-                b[i][j]='Q'
+                b[i][j]="Q"
                 row.add(i)
                 col.add(j)
                 pos.add(i+j)
@@ -22,7 +20,7 @@ class Solution:
                 pos.discard(i+j)
                 neg.discard(i-j)
                 b[i][j]='.'
-            
+
 
         sol(0)
         return res

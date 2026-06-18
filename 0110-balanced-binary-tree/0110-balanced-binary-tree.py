2eee4a8cfa5ff41c52ab119da0403d26a8028668
Priti Prabha <jdwomenscoll@gmail.com>
2026-06-18 15:10:50 +0530

    Time: 3 ms (53.06%), Space: 20.3 MB (70.62%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -9,16 +9,16 @@ class Solution:
         
         def height(root):
             if not root:
-                return 0
+                return False
             l=height(root.left)
             if l==-1:
                 return -1
-            
             r=height(root.right)
             if r==-1:
                 return -1
             if abs(l-r)>1:
                 return -1
-            
             return 1+max(l,r)
+            
         return height(root)!=-1
+        

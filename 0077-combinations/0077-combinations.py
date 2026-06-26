5e94a2a5d48630fb6a0dd9c17e52ec6b0450e9d4
Priti Prabha <jdwomenscoll@gmail.com>
2026-06-26 14:54:09 +0530

    Time: 135 ms (19.89%), Space: 61.5 MB (26.51%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,17 +1,17 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        # return list(combinations(range(1,n+1),k))
         res=[]
-        nums=[i for i in range(1,n+1)]
-        def com(i,temp,k):
+        nums=list(range(1,n+1))
+        def sol(i,k,temp):
             if k==0:
                 res.append(temp[:])
                 return
             if i==len(nums):
                 return
             temp.append(nums[i])
-            com(i+1,temp,k-1)
+            sol(i+1,k-1,temp)
             temp.pop()
-            com(i+1,temp,k)
-        com(0,[],k)
+            sol(i+1,k,temp)
+
+        sol(0,k,[])
         return res

ef1db51fcf18d97800339ab2f4f4b6392d4d383d
Priti Prabha <jdwomenscoll@gmail.com>
2026-07-16 08:48:56 +0530

    Time: 0 ms (100%), Space: 19.4 MB (27.36%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,11 +1,13 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res=[]
-        def sol(i,temp):
-            res.append(temp[:])
-            for j in range(i,len(nums)):
-                temp.append(nums[j])
-                sol(j+1,temp)
-                temp.pop()
-        sol(0,[])
-        return res
+        ans=[]
+        def solve(idx,subset):
+            if idx==len(nums):
+                ans.append(subset.copy())
+                return
+            subset.append(nums[idx])
+            solve(idx+1,subset)
+            subset.pop()
+            solve(idx+1,subset)
+        solve(0,[])
+        return ans

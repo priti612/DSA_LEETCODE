b841e4b4e49cd74e1fb6973573e30fa9bec274ba
Priti Prabha <jdwomenscoll@gmail.com>
2026-07-20 01:07:32 +0530

    Time: 0 ms (100%), Space: 19.6 MB (22.15%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,19 +1,19 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        
         res=[]
         nums.sort()
-        def sol(i,temp):
-            if temp[:] not in res:
-                res.append(temp[:])
-            for j in range(i,len(nums)):
-                if j!=i and nums[j]==nums[j-1]:
-                    continue 
-                
-                temp.append(nums[j])
-                sol(j+1,temp)
-                temp.pop()
-                
-
-
+        def sol(idx,subset):
+            if idx==len(nums):
+                res.append(tuple(subset))
+                return
+            
+            subset.append(nums[idx])
+            sol(idx+1,subset)
+            subset.pop()
+            while idx+1<len(nums) and nums[idx]==nums[idx+1]:
+                idx+=1
+            sol(idx+1,subset)
+            
         sol(0,[])
-        return res
+        return [list(i) for i in res]

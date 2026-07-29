5b41915f6998de2b880856c3ed5862c937e9297f
Priti Prabha <jdwomenscoll@gmail.com>
2026-07-29 13:31:56 +0530

    Time: 0 ms (100%), Space: 19.3 MB (18.41%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.py
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.py
@@ -1,8 +1,11 @@
 class Solution:
     def buyChoco(self, prices: List[int], money: int) -> int:
-        res=-1
-        for i in range(len(prices)):
-            for j in range(i+1,len(prices)):
-                if prices[i]+prices[j]<=money:
-                    res=max(res,money-prices[i]-prices[j])
-        return res if res!=-1 else money
+        # res=-1
+        # for i in range(len(prices)):
+        #     for j in range(i+1,len(prices)):
+        #         if prices[i]+prices[j]<=money:
+        #             res=max(res,money-prices[i]-prices[j])
+        # return res if res!=-1 else money
+        prices.sort()
+        buy=prices[0]+prices[1]
+        return money if buy>money else money-buy

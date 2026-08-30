ffd5ccdeb519b696e7de7fb9c6c62fe56c4191be
Priti Prabha <jdwomenscoll@gmail.com>
2026-08-30 19:17:19 +0530

    Time: 123 ms (33.4%), Space: 23.6 MB (32.01%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.py
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.py
@@ -1,16 +1,18 @@
 class Solution:
-    def bestClosingTime(self, s: str) -> int:
-        sc=0
-        maxi=0
-        best=0
-
-        for i,char in enumerate(s):
-            if char=='Y':
-                sc+=1
-                
-            else:
-                sc-=1
-            if sc>maxi:
-                maxi=sc
-                best=i+1
-        return best
+    def bestClosingTime(self, customers: str) -> int:
+        n=len(customers)
+        suff=[0]*(n+1)
+        for i in range(n):
+            suff[i+1]=suff[i]+(1 if customers[i]=='N' else 0)
+        nfor=0
+        mini=float('inf')
+        b=0
+        for i in range(n,-1,-1):
+            p=suff[i]+nfor
+            if(p<=mini):
+                mini=p
+                b=i
+            if i>0 and customers[i-1]=='Y':
+                nfor+=1
+        return b
+        

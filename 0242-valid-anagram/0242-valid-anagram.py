3600cdd9265087608413642d23877196df2fc757
Priti Prabha <jdwomenscoll@gmail.com>
2026-09-23 19:09:16 +0530

    Time: 7 ms (91.94%), Space: 19.5 MB (29.21%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,17 +1,5 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-
-
-        f1={}
-        for i in s:
-            f1[i]=f1.get(i,0)+1
-        f2={}
-        for i in t:
-            f2[i]=f2.get(i,0)+1
-        if f1==f2:
-            return True
-        return False
-
-          
-          
-       
+        if len(s)!=len(t):
+            return False
+        return Counter(s)==Counter(t)

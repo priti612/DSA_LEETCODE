3beaa5feb7e52d13fe5cdd9f67bd3c47e8d008ee
Priti Prabha <jdwomenscoll@gmail.com>
2026-09-23 18:56:51 +0530

    Time: 0 ms (100%), Space: 52 MB (42.06%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.cs
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cs
@@ -1,10 +1,13 @@
 public class Solution {
     public int[] GetConcatenation(int[] nums) {
-       int n=nums.Length;
-       int[] ans=new int[2*n];
-       for(int i=0;i<n;i++){
-        ans[i]=ans[i+n]=nums[i];
-       }
-       return ans;
+        int n=nums.Length;
+        int [] ans=new int[2*n];
+        
+        for(int i=0;i<nums.Length;i++){
+            ans[i]=nums[i];
+            ans[i+n]=nums[i];
+        }
+        
+        return ans;
     }
 }
